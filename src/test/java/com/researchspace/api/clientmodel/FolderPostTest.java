d6afdc2129dfaedba6f0cf369bfc53aceefe9ce2
Richard Adams <ra22597@gmail.com>
2018-03-06 17:15:41 +0000

    Folder object

--- src/test/java/com/researchspace/api/clientmodel/FolderPostTest.java
+++ src/test/java/com/researchspace/api/clientmodel/FolderPostTest.java
@@ -2,11 +2,19 @@ package com.researchspace.api.clientmodel;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class FolderPostTest {
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+public class FolderPostTest extends AbstractModelTest {
+	
+	File FolderJson = new File("src/test/resources/Folder.json");
 
 	@Before
 	public void setUp() throws Exception {
@@ -17,8 +25,16 @@ public class FolderPostTest {
 	}
 
 	@Test
-	public void test() {
+	public void folderPost() {
 		assertEquals("Test folder", FolderPost.builder().name("Test folder").build().getName());
+	}
+	
+	@Test
+	public void folderReat() throws JsonParseException, JsonMappingException, IOException {
+		Folder folder = readFileToClass(FolderJson, Folder.class);
+		assertTrue(folder.isNotebook());
+		assertEquals(295, folder.getId().longValue());
+		assertEquals(271, folder.getParentFolderId().longValue());
 	}
 
 }
