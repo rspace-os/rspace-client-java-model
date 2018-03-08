1af0e4eefa036e8034de201eed3c91504cb8aeb9
Richard Adams <ra22597@gmail.com>
2018-03-08 09:33:19 +0000

    Add parent folder id choice when creating a folder

--- src/main/java/com/researchspace/api/clientmodel/FolderPost.java
+++ src/main/java/com/researchspace/api/clientmodel/FolderPost.java
@@ -6,8 +6,17 @@ import lombok.Data;
 @Data
 @Builder
 public class FolderPost {
-	
+	/**
+	 * The name of the folder or notebook to be created
+	 */
 	private String name;
+	/**
+	 * <code>true</code> if creating a notebook, <code>false</code>otherwise.
+	 */
 	private Boolean notebook;
+	/**
+	 * An id for the folder in which to create the notebook.
+	 */
+	private Long parentFolderId;
 
 }
