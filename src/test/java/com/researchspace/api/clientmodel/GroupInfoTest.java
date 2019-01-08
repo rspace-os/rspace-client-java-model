37e2507a70ec2f213a308394ff9f6afc37815006
Richard Adams <ra22597@gmail.com>
2019-01-08 19:02:49 +0000

    new test

---
+++ src/test/java/com/researchspace/api/clientmodel/GroupInfoTest.java
@@ -0,0 +1,15 @@
+package com.researchspace.api.clientmodel;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class GroupInfoTest {
+
+	@Test
+	public void test() {
+		GroupInfo g = new GroupInfo();
+		assertNotNull(g);
+	}
+
+}
