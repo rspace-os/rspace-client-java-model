dd3eaccb32777b7db5a2308a9723469f0fbfc7e8
Richard Adams <ra22597@gmail.com>
2019-01-10 16:09:21 +0000

    sharing post

---
+++ src/main/java/com/researchspace/api/clientmodel/SharePost.java
@@ -0,0 +1,31 @@
+package com.researchspace.api.clientmodel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SharePost {
+ 
+  @Size(min=1,max=255, message="There must be at least 1 document or notebook to share")
+  @Singular("itemToShare")
+  private List<Long> itemsToShare = new ArrayList<>();
+  
+  @Valid
+  @Singular
+  private List<GroupSharePostItem> groupSharePostItems = new ArrayList<>();
+  @Valid
+  @Singular
+  private List<UserSharePostItem> userSharePostItems = new ArrayList<>();
+}
