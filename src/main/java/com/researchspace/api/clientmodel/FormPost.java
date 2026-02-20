1e0aa2f9e7b0a5ccf77569027e3083184e0f8eee
Maciej Kowalski <mKowalski256@users.noreply.github.com>
2026-02-20 11:02:19 +0000

    1.99.0: switch from rspace-os-parent to rspace-parent as parent pom (#12)

--- src/main/java/com/researchspace/api/clientmodel/FormPost.java
+++ src/main/java/com/researchspace/api/clientmodel/FormPost.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
